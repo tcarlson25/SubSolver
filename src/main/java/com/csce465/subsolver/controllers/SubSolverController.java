997a8727a5bf7ee7000e04c3b7441c76c1ff77ad
Tyler <tcarlson25@tamu.edu>
2018-10-23 17:35:04 -0500

    home screen is not at /home

--- src/main/java/com/csce465/subsolver/controllers/SubSolverController.java
+++ src/main/java/com/csce465/subsolver/controllers/SubSolverController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SubSolverController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/home")
 	public String home(Map<String, Object> model) {
 		model.put("message", "Welcome Screen!");
 		return "home";
